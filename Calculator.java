a6be3242021e9f82544583884026922cae3e254b
JosiahD1234 <jwdegeneffe@cpp.edu>
2025-01-28 20:48:22 -0800

    first commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double result;
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        num1 = scnr.nextDouble();
+        System.out.println("Enter next number:");
+        num2 = scnr.nextDouble();
+        result = num1 + num2;
+        System.out.println("Result: " + result);
+        scnr.close();
+    }
+}
